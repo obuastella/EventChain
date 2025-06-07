42f3226f81a91e0392017bb997ad88f645276000
Obua Stella <stellaobua123@gmail.com>
2025-06-07 09:27:36 +0100

    Landing page

---
+++ src/pages/LandingPage/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="mt-28 m-4">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2025{" "}
+          <Link to="/" className="hover:underline">
+            MetaTicket.
+          </Link>
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Privacy Policy
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline me-4 md:me-6">
+              Whitepaper
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
 
 export default function Index() {
   return (
-    <>
+    <div className="">
       <Navbar />
       <Hero />
-    </>
+      <Footer />
+    </div>
   );
 }
