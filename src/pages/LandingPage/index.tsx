ffb914a892844cb75e968ed2548e3234497d96ea
Obua Stella <stellaobua123@gmail.com>
2025-06-07 09:33:05 +0100

    updated folder

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default function Index() {
   return (
